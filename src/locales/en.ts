f447092399e7b428515b73837ed0796c3c5c5acc
Ninhache <mr.carambar27@gmail.com>
2024-06-05 17:40:36 +0200

    feat: improve both files to have more translate

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -5,6 +5,139 @@ export default {
     login: "Login",
     dashboard: "Dashboard",
   },
+  about: {
+    title: "About",
+    abstract: "Learn about the tech stack and inspiration behind {name}.",
+  },
+  changelog: {
+    title: "Changelog",
+    abstract: "All the latest updates, improvements, and fixes.",
+  },
+  login: {
+    welcomeBack: "Welcome Back 👋",
+    logout: "Logout",
+  },
+  goBack: {
+    back: "Back",
+  },
+  settings: {
+    upload: {
+      success: "Updated successfully!",
+      error: "Something went wrong.",
+      loading: "Updating...",
+      save: "Update",
+    },
+    uploadAvatar: "Click on the avatar to upload new one.",
+    form: {
+      name: "Name",
+      namePlaceholder: "Your name",
+      email: "Email",
+      emailPlaceholder: "Your email address",
+    },
+  },
+  navBar: {
+    projects: "Projects",
+    billing: "Billing",
+    settings: "Settings",
+  },
+  imageUpload: {
+    success: "Uploaded successfully!",
+    error: {
+      spaceInNameCode: "Spaces in file name",
+      spaceInNameDesc: "Spaces in file names are not acceptable!",
+      unknown: "Error occurred while uploading!",
+      spaceInFileDesc: "File name with spaces is not acceptable",
+      sizeExceed: "Only Images are supported. Max file size is {size}.",
+    },
+    label: "Image Upload",
+    cancel: "Cancel",
+    loading: "Uploading...",
+    dropHere: "Drop Here",
+    or: "OR",
+    clickHere: "Click here",
+    upload: "Upload",
+  },
+  confirmModal: {
+    cancel: "Cancel",
+    ignore: "Are you sure to discard the changes?",
+    yes: "Yes",
+    no: "No",
+  },
+  authForm: {
+    error: "Failed to send Magic link!",
+    tryAgain: "Please try again later.",
+    success: "Magic Link sent!",
+    successAdditionalInfos: "Please check your mail inbox",
+    form: {
+      email: "Email",
+      emailPlaceholder: "name@example.com",
+      submit: "Send magic link",
+    },
+    continueWithGit: "Continue with {github}",
+  },
+  projects: {
+    details: "View project details",
+    create: {
+      label: "Create a project",
+      save: "Create",
+      success: "Project created successfully",
+      error: {
+        limitReached: "Free plan limit reached. Please upgrade your plan.",
+        unknown: "Error creating project. Please try again",
+      },
+      form: {
+        name: "Name",
+        namePlaceholder: "XYZ",
+        domain: "Domain",
+        domainPlaceholder: "xyz.com",
+      },
+    },
+
+    update: {
+      success: "Project Updated successfully.",
+      error: {
+        unknown: "Error updating your project.",
+      },
+      tryAgain: "Please try again.",
+      save: "Save",
+      form: {
+        id: "ID",
+        name: "Name",
+        namePlaceholder: "XYZ",
+        domain: "Domain",
+        domainPlaceholder: "xyz.com",
+      },
+    },
+    delete: {
+      label: "Delete Project",
+      confirmationQuestion: "Are you absolutely sure?",
+      confirmationDescription:
+        "The project will be permanently deleted. This action is irreversible and can not be undone.",
+      success: "Project deleted successfully",
+      error: {
+        unknown: "Error deleting project.",
+      },
+      tryAgain: "Please try again.",
+      cancel: "Cancel",
+      delete: "Delete",
+    },
+    tabs: {
+      details: "Details",
+      settings: "Settings",
+    },
+  },
+  billing: {
+    alertDescription:
+      "{name} just demonstrates how to use Stripe in Next.js App router. Please use test cards from {link}",
+    title: "Subscription",
+    abstract: "Currently on the {name} plan.",
+    manageSubscription: "Manage Subscription",
+    upgradeToPro: "Upgrade to PRO",
+    endPlan: "Your plan will be canceled on ",
+    renewPlan: "Your plan renews on ",
+    error: "Something went wrong.",
+    tryAgain: "Please refresh the page and try again.",
+  },
   hero: {
     top: "Introducing",
     main: "Quick Starter Template for your Next project",
@@ -12,5 +145,61 @@ export default {
     firstButton: "Get started",
     tools: "Built using Great Tools",
     on: "on",
+  },
+  features: {
+    title: "Features",
+    abstract:
+      "This template comes with features like Authentication, API routes, File uploading and more in Next.js App dir.",
+    next: "App dir, Routing, Layouts, API routes, Server Components, Server actions.",
+    shadcn: "UI components built using Radix UI and styled with Tailwind CSS.",
+    prisma: "Using Postgres with Prisma ORM, hosted on Vercel Postgres.",
+    lucia: "Authentication and Authorization using LuciaAuth v3.",
+    uploadThings: "Upload and preview files effortlessly with UploadThing.",
+    reactEmail: "Create emails using React Email and Send with Resend.",
+    i18n: "Internationalization support with type-safe Next-International.",
+    stripe: "Receive and process payments with Stripe.",
+    vercel: "Production and Preview deployments with Vercel.",
+    additionalInfo:
+      "{name} also includes Changelog & About page built using {component} and Markdown.",
+  },
+  pricing: {
+    title: "Pricing",
+    abstract: "Choose the plan that’s right for you and start enjoying it all.",
+    free: "Free",
+    freeDescription: "Up to 3 projects",
+    pro: "Pro",
+    proDescription: "Unlimited projects",
+    month: "month",
+    getStarted: "Get Started",
+    managePlan: "Manage Plan",
+    upgradePlan: "Upgrade Plan",
+    current: "Current",
+  },
+  openSource: {
+    title: "Proudly Open Source",
+    abstract:
+      "{name} is open source and powered by open source software. The code is available on GitHub.",
+    starMe: "Start me",
+  },
+  error: {
+    source: "Oops, Something Went Wrong!",
+    retry: "Try Again",
+  },
+
+  footer: {
+    developedBy: "Developed by {name}",
+  },
+  toggle: {
+    light: "Light",
+    dark: "Dark",
+    system: "System",
+    labelTheme: "Toggle theme",
+    labelLang: "Changer de langue",
+  },
+  copy: {
+    error: "Error copying!",
+    success: "Project ID copied!",
+    tryAgain: "Please try again.",
+    label: "Copy",
   },
 } as const;
--- src/locales/fr.ts
+++ src/locales/fr.ts
@@ -1,16 +1,213 @@
 export default {
   header: {
-    changelog: "Journal des modifications",
-    about: "Environ",
+    changelog: "Changelog",
+    about: "À propos",
     login: "Se connecter",
     dashboard: "Tableau de bord",
   },
+  about: {
+    title: "À propos",
+    abstract: "Découvrez les inspirations derrière {name} !",
+  },
+  changelog: {
+    title: "Changelog",
+    abstract:
+      "Toutes les dernières mises à jour, améliorations et corrections.",
+  },
+  login: {
+    welcomeBack: "Bon retour 👋",
+    logout: "Se déconnecter",
+  },
+  goBack: {
+    back: "Retour",
+  },
+  settings: {
+    upload: {
+      success: "Mis à jour avec succès !",
+      error: "Une erreur est survenue.",
+      loading: "Mise à jour...",
+      save: "Sauvegarder",
+    },
+    uploadAvatar: "Cliquez sur l'avatar pour en télécharger un nouveau.",
+    form: {
+      name: "Nom",
+      namePlaceholder: "Votre nom",
+      email: "Email",
+      emailPlaceholder: "Votre adresse email",
+    },
+  },
+  navBar: {
+    projects: "Projets",
+    billing: "Tarifs",
+    settings: "Paramètres",
+  },
+  imageUpload: {
+    success: "Téléchargé avec succès !",
+    error: {
+      spaceInNameCode: "Espaces dans le nom de fichier",
+      spaceInNameDesc:
+        "Les espaces dans les noms de fichiers ne sont pas acceptés !",
+      unknown: "Erreur lors du téléchargement !",
+      spaceInFileDesc:
+        "Le nom de fichier avec des espaces n'est pas acceptable",
+      sizeExceed:
+        "Seules les images sont prises en charge. La taille maximale du fichier est de {size}.",
+    },
+    label: "Téléchargement d'image",
+    cancel: "Annuler",
+    loading: "Téléchargement...",
+    dropHere: "Déposez ici",
+    or: "OU",
+    clickHere: "Cliquez ici",
+    upload: "Télécharger",
+  },
+  confirmModal: {
+    cancel: "Cancel",
+    ignore: "Êtes-vous sûr d'ignorer les modifications ?",
+    yes: "Oui",
+    no: "Non",
+  },
+  authForm: {
+    error: "Échec de l'envoi du lien magique !",
+    tryAgain: "Veuillez réessayer plus tard.",
+    success: "Lien magique envoyé !",
+    successAdditionalInfos: "Veuillez vérifier votre boîte mail",
+    form: {
+      email: "Email",
+      emailPlaceholder: "nom@exemple.com",
+      submit: "Envoyer le lien magique",
+    },
+    continueWithGit: "Continuer avec {github}",
+  },
+  projects: {
+    details: "Voir les détails du projet",
+    create: {
+      label: "Créer un projet",
+      save: "Créer",
+      success: "Projet crée avec succès",
+      error: {
+        limitReached:
+          "Votre abonnement atteint ses limites ! Passez à l'abonnement pro !",
+        unknown: "Une erreur est survenue, veuillez réessayer",
+      },
+      form: {
+        name: "Nom",
+        namePlaceholder: "XYZ",
+        domain: "Domaine",
+        domainPlaceholder: "xyz.com",
+      },
+    },
+    update: {
+      success: "Projet modifié avec succès.",
+      error: {
+        unknown: "Une erreur est survenue en voulant sauvegarder votre projet.",
+      },
+      tryAgain: "Veuillez réessayer.",
+      save: "Sauvegarder",
+      form: {
+        id: "ID",
+        name: "Nom",
+        namePlaceholder: "XYZ",
+        domain: "Domaine",
+        domainPlaceholder: "xyz.com",
+      },
+    },
+    delete: {
+      label: "Supprimer le projet",
+      confirmationQuestion: "Êtes-vous absolument sûr ?",
+      confirmationDescription:
+        "Le projet sera supprimé définitivement. Cette action est irréversible et ne peut pas être annulée.",
+      success: "Projet supprimé avec succès",
+      error: {
+        unknown: "Erreur lors de la suppression du projet.",
+      },
+      tryAgain: "Veuillez réessayer.",
+      cancel: "Annuler",
+      delete: "Supprimer",
+    },
+    tabs: {
+      details: "Détails",
+      settings: "Paramètres",
+    },
+  },
+  billing: {
+    alertDescription:
+      "{name} montre juste comment utiliser Stripe avec Next.js et son App router. Veuillez utiliser les cartes de test de {link}",
+    title: "Abonnement",
+    abstract: "Actuellement sur le plan {name}.",
+    manageSubscription: "Gérer l'abonnement",
+    upgradeToPro: "Passer à PRO",
+    endPlan: "Votre abonnement sera annulé le ",
+    renewPlan: "Votre abonnement sera renouvelé le ",
+    error: "Quelque chose n'a pas fonctionné.",
+    tryAgain: "Veuillez rafraichir la page et réessayer.",
+  },
   hero: {
     top: "Présentation de",
-    main: "Modèle de démarrage rapide pour votre prochain projet",
+    main: "Kit de démarrage rapide pour votre prochain projet",
     sub: "Doté de toutes les fonctionnalités nécessaires pour commencer.",
     firstButton: "Commencer",
     tools: "Construit à l'aide d'excellents outils",
     on: "sur",
+  },
+  features: {
+    title: "Fonctionnalités",
+    abstract:
+      "Ce kit arrive avec des fonctionnalités telles que la gestion d'utilisateur, le routage simple d'API, le téléchargement de fichiers et plus encore.",
+    next: "Boostez votre SEO avec les optimisations de NextJS.",
+    shadcn:
+      "Facilitez la création de pages avec des composants façonnés avec Radix UI et stylisez-les avec Tailwind CSS.",
+    prisma:
+      "Gérez vos données avec une base Postgres et Prisma, le tout hébergé sur Vercel Postgres.",
+    lucia:
+      "Gérez les rôles et l'authentification de vos utilisateurs avec LuciaAuth v3.",
+    uploadThings:
+      "Téléchargez et prévisualisez des fichiers sans effort avec UploadThing.",
+    reactEmail: "Créez des emails avec React Email et envoyez-les avec Resend.",
+    i18n: "Supportez l'internationalisation avec la robustesse de Next-International.",
+    stripe: "Recevez et traitez les paiements avec Stripe.",
+    vercel: "Déployez en production et en prévisualisation avec Vercel.",
+    additionalInfo:
+      "{name} inclut également des pages de changelog et d'À propos construites avec {component} et du Markdown.",
+  },
+  pricing: {
+    title: "Tarifs",
+    abstract:
+      "Choisissez le plan qui vous convient et commencez à en profiter.",
+    free: "Free",
+    freeDescription: "Jusque 3 projets",
+    pro: "Business",
+    proDescription: "Projets illimités ",
+    month: "mois",
+    getStarted: "Commencer",
+    managePlan: "Gérer votre abonnement",
+    upgradePlan: "Passer à la version supérieure",
+    current: "Actuellement",
+  },
+  openSource: {
+    title: "Fièrement Open Source",
+    abstract:
+      "{name} est un logiciel open source, alimenté par des outils open source. Le code est disponible sur GitHub.",
+    starMe: "Mettez moi une étoile",
+  },
+  error: {
+    source: "Oops, quelque chose s'est mal passé!",
+    retry: "On réessaye ?",
+  },
+  footer: {
+    developedBy: "Développé par {name}",
+  },
+  toggle: {
+    light: "Clair",
+    dark: "Sombre",
+    system: "Système",
+    labelTheme: "Changer de thème",
+    labelLang: "Changer de langue",
+  },
+  copy: {
+    error: "Erreur de copie!",
+    success: "L'Id du projet a été copié!",
+    tryAgain: "Veuillez réessayer.",
+    label: "Copier",
   },
 } as const;
